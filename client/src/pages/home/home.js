5038e630f8b3d313af064b961edc9ad6879d0202
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-09-12 20:52:30 -0300

    feat: update home component to consume api

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,15 +1,37 @@
 import { Card } from "../../components/card/card";
+import { api } from "../../utils/api/api";
+import { useState, useEffect } from "react";
 import "./home.css";
 export function Home() {
+  const [animeList, setAnimeList] = useState([]);
+
+  async function getAnime() {
+    const animes = await api.getAllAnimes();
+    setAnimeList(animes);
+  }
+
+  // executa novamente toda vez que um state for alterado
+  //   useEffect(() => {
+  //     getAnime();
+  //   });
+
+  // executa somente quando o componente for renderizado pois não há nada no array de dependencias
+  useEffect(() => {
+    getAnime();
+  }, []);
+
+  // executa somente quando a(s) dependencia(s) do array tiverem alguma mutação
+  //   useEffect(() => {
+  //     getAnime();
+  //   }, [animeList]);
+
+  console.log("Renderizou");
   return (
     <>
       <div className="card-list">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {animeList.map((item, index) => {
+          return <Card key={index} />;
+        })}
       </div>
     </>
   );
