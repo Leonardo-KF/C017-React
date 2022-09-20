0c1a79813092bdab056603e8c61da123ba46490e
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-09-20 20:58:28 -0300

    feat: add badge and remove button

--- el-geladon/src/components/PaletaLista/PaletaLista.jsx
+++ el-geladon/src/components/PaletaLista/PaletaLista.jsx
@@ -1,22 +1,64 @@
 import "./PaletaLista.css";
 import { paletas } from "../../mocks/paletas";
+import { useState } from "react";
 
 export function PaletaLista() {
+  const [paletaSelecionada, setPaletaSelecionada] = useState({});
+
+  const adicionarPaleta = (paletaIndex) => {
+    const paleta = {
+      [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) + 1,
+    };
+
+    setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
+  };
+
+  const removerPaleta = (paletaIndex) => {
+    const paleta = {
+      [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) - 1,
+    };
+
+    setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
+  };
+
+  const removeButton = (canReader, index) =>
+    Boolean(canReader) && (
+      <button className="Acoes__remover" onClick={() => removerPaleta(index)}>
+        remover
+      </button>
+    );
+
+  const badgeCounter = (canReader, index) =>
+    Boolean(canReader) && (
+      <span className="PaletaListaItem__badge">{paletaSelecionada[index]}</span>
+    );
+
   return (
     <div className="PaletaLista">
       {paletas.map((paleta, index) => (
         <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
+          {badgeCounter(paletaSelecionada[index], index)}
           <div>
             <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
-            <div className="PaletaListaItem__preco">R$ {paleta.preco.toFixed(2)}</div>
+            <div className="PaletaListaItem__preco">
+              R$ {paleta.preco.toFixed(2)}
+            </div>
             <div className="PaletaListaItem__descricao">{paleta.descricao}</div>
             <div className="PaletaListaItem__acoes Acoes">
-              <button className="Acoes__adicionar Acoes__adicionar--preencher">
+              <button
+                className={`Acoes__adicionar ${
+                  !paletaSelecionada[index] && "Acoes__adicionar--preencher"
+                }`}
+                onClick={() => adicionarPaleta(index)}
+              >
                 adicionar
               </button>
+
+              {removeButton(paletaSelecionada[index], index)}
             </div>
           </div>
           <img
+            className="PaletaListaItem__foto"
             src={paleta.foto}
             alt={`Paleta de ${paleta.sabor}`}
           />
