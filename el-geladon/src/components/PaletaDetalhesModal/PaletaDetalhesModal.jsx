af8f6ef3097221102bf6901e0f8607729830d9dc
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-09-29 20:47:36 -0300

    feat: add PaletaDetalhesModal component

---
+++ el-geladon/src/components/PaletaDetalhesModal/PaletaDetalhesModal.jsx
@@ -0,0 +1,25 @@
+import "./PaletaDetalhesModal.css";
+import { Modal } from "../Modal/Modal";
+
+export function PaletaDetalhesModal({ paleta, closeModal }) {
+  return (
+    <Modal closeModal={closeModal}>
+      <div className="PaletaDetalhesModal">
+        <div>
+          <div className="PaletaDetalhesModal__titulo">{paleta.titulo}</div>
+          <div className="PaletaDetalhesModal__preco">
+            R$ {Number(paleta.preco).toFixed(2)}
+          </div>
+          <div className="PaletaDetalhesModal__descricao">
+            {paleta.descricao}
+          </div>
+          <img
+            src={paleta.foto}
+            alt={`Paleta de ${paleta.sabor}`}
+            className="PaletaDetalhesModal__foto"
+          />
+        </div>
+      </div>
+    </Modal>
+  );
+}
