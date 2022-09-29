742948a856bb7807f67893f05a18dc4820f2322e
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-09-29 20:36:22 -0300

    feat: add modal

---
+++ el-geladon/src/components/Modal/Modal.jsx
@@ -0,0 +1,22 @@
+import "./Modal.css";
+import { Overlay } from "../Overlay/Overlay";
+
+export function Modal({ children, closeModal }) {
+  const handleClick = (e, canClose) => {
+    e.stopPropagation();
+    if (canClose) {
+      closeModal();
+    }
+  };
+
+  return (
+    <Overlay overlayClick={closeModal}>
+      <div className="Modal">
+        <span className="Modal__close" onClick={(e) => handleClick(e, true)}>
+          +
+        </span>
+        <div className="Modal__body">{children}</div>
+      </div>
+    </Overlay>
+  );
+}
