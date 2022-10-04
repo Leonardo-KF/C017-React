5ce119212e00f193af008350e1bda886404ca64c
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-10-04 20:39:00 -0300

    feat: add create paleta button

--- el-geladon/src/components/Header/Header.jsx
+++ el-geladon/src/components/Header/Header.jsx
@@ -2,8 +2,9 @@ import "./Header.css";
 
 import sacola from "../../assets/icons/sacola.svg";
 import logo from "../../assets/logo.svg";
+import paleta from "../../assets/icons/paleta.svg";
 
-export function Header() {
+export function Header({ createPaleta }) {
   return (
     <div className="Header">
       <div className="row">
@@ -16,14 +17,28 @@ export function Header() {
           />
           <span className="Logo__titulo">El Geladon</span>
         </div>
-        <div className="Header__opcoes Opcoes"></div>
-        <div className="Opcoes__sacola Sacola">
-          <img
-            src={sacola}
-            alt="Sacola de Compras"
-            width="40px"
-            className="Sacola__icone"
-          />
+        <div className="Header__opcoes Opcoes">
+          <button
+            type="button"
+            className="Opcoes__paleta Paleta"
+            onClick={() => createPaleta()}
+          >
+            <img
+              src={paleta}
+              alt="Adicionar paleta"
+              className="Paleta__icone"
+              width="40px"
+            />
+          </button>
+
+          <div className="Opcoes__sacola Sacola">
+            <img
+              src={sacola}
+              alt="Sacola de Compras"
+              width="40px"
+              className="Sacola__icone"
+            />
+          </div>
         </div>
       </div>
     </div>
